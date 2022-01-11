b879d5a680cbff02025277b04422c9ff91371fb6
Israel Miller <ismille@microsoft.com>
2022-01-11 16:35:39 -0500

    making config based

--- cmd/root.go
+++ cmd/root.go
@@ -58,8 +58,6 @@ func initConfig() {
 		viper.SetConfigName(".draftv2")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
-
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
---
+++ pkg/configs/createConfig.go
@@ -0,0 +1,13 @@
+package configs
+
+type CreateConfig struct {
+	DeployType        string
+	LanguageType      string
+	DeployVariables   []UserInputs
+	LanguageVariables []UserInputs
+}
+
+type UserInputs struct {
+	Name  string
+	Value string
+}

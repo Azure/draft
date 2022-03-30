53c0c9527b0229ab85b21c3379abb95b3ba4c1c1
eborders <v-eborders@microsoft.com>
2022-03-30 18:36:21 -0400

    added prompt support if no flags are set

--- cmd/setup-gh.go
+++ cmd/setup-gh.go
@@ -22,6 +22,8 @@ import (
 
 	"github.com/Azure/draftv2/pkg/providers"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/manifoldco/promptui"
 )
 
 
@@ -35,17 +37,22 @@ func newSetUpCmd() *cobra.Command {
 		Long: `This command automates the process of setting up Github OIDC by creating an Azure Active Directory application 
 		and service principle, and configuring that application to trust github`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			
-			if err := hasValidProviderInfo(sc); err != nil {
-				return err
+			// TODO: check if flags are set - if not, run prompts
+			if !flagsAreSet(cmd.Flags()) {
+				gatherUserInfo(sc)
+				fmt.Printf("%v", sc)
+			} else {
+				if err := hasValidProviderInfo(sc); err != nil {
+					return err
+				}
+	
 			}
-
+			
 			if err := runProviderSetUp(sc); err != nil {
 				return err
 			}
-
-			return nil
 			
+			return nil		
 		},
 	}
 
@@ -69,7 +76,6 @@ func hasValidProviderInfo(sc *providers.SetUpCmd) error {
 }
 
 
-
 func runProviderSetUp(sc *providers.SetUpCmd) error {
 	provider := strings.ToLower(sc.Provider)
 	if provider == "azure" {
@@ -82,6 +88,102 @@ func runProviderSetUp(sc *providers.SetUpCmd) error {
 	}
 
 	return nil
+}
+
+func flagsAreSet(f *pflag.FlagSet) bool {
+	return f.Changed("subscription-id")
+}
+
+func getAppName() string {
+	validate := func(input string) error {
+		if input == "" {
+			return errors.New("Invalid app name")
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    "Enter app name",
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return err.Error()
+	}
+
+	return result
+}
+
+func getSubscriptionID() string {
+	validate := func(input string) error {
+		// TODO: check if it's an existing subscription id
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    "Enter subscription ID",
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return err.Error()
+	}
+
+	return result
+}
+
+func getResourceGroup() string {
+	validate := func(input string) error {
+		if input == "" {
+			return errors.New("Invalid resource group name")
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    "Enter resource group name",
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return err.Error()
+	}
+
+	return result
+}
+
+func getCloudProvider() string {
+	selection := &promptui.Select{
+		Label: "What cloud provider would you like to use?",
+		Items: []string{"azure"},
+	}
+
+	_, selectResponse, err := selection.Run()
+	if err != nil {
+		return err.Error()
+	}
+
+	return selectResponse
+}
+
+func gatherUserInfo(sc *providers.SetUpCmd) {
+	if getCloudProvider() == "azure" {
+		sc.AppName = getAppName()
+		sc.SubscriptionID = getSubscriptionID()
+		sc.ResourceGroupName = getResourceGroup()
+	} else {
+		// prompts for other cloud providers
+	}
+
 }
 
 

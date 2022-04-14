59d906304ef5b46fa44c028489784a2161e74823
Karen Chen <karenchen@microsoft.com>
2022-04-13 17:02:46 -0700

    better help menu + typo fix

--- cmd/generate-workflow.go
+++ cmd/generate-workflow.go
@@ -10,9 +10,10 @@ func newGenerateWorkflowCmd() *cobra.Command {
 	workflowConfig := &workflows.WorkflowConfig{}
 	dest := ""
 	var cmd = &cobra.Command{
-		Use:   "generate-workflow",
-		Short: "generates a github workflow for automatic build and deploy to AKS",
-		Long:  `This command will generate a github workflow to build and deploy an application containerized with draft. This command assumes the 'setup-gh' command has been run properly.'`,
+		Use:   "generate-workflow [flags]",
+		Short: "Generates a github workflow for automatic build and deploy to AKS",
+		Long:  `This command will generate a github workflow to build and deploy an application containerized 
+with draft on AKS. This command assumes the 'setup-gh' command has been run properly.'`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			workflowConfig.ValidateAndFillConfig()
 
@@ -21,11 +22,11 @@ func newGenerateWorkflowCmd() *cobra.Command {
 	}
 
 	f := cmd.Flags()
-	f.StringVarP(&workflowConfig.AksClusterName, "cluster-name", "c", "", "name of AKS cluster")
-	f.StringVarP(&workflowConfig.AcrName, "registry-name", "r", "", "the Azure container registry name")
-	f.StringVar(&workflowConfig.ContainerName, "container-name", "", "the name of the container image")
-	f.StringVarP(&workflowConfig.ResourceGroupName, "resource-group", "g", "", "the Azure resource group of your AKS cluster")
-	f.StringVarP(&dest, "destination", "d", ".", "root of repository for gh workflow")
+	f.StringVarP(&workflowConfig.AksClusterName, "cluster-name", "c", "", "Specify the name of the AKS cluster")
+	f.StringVarP(&workflowConfig.AcrName, "registry-name", "r", "", "Specify the Azure container registry name")
+	f.StringVar(&workflowConfig.ContainerName, "container-name", "", "Specify the name of the container image")
+	f.StringVarP(&workflowConfig.ResourceGroupName, "resource-group", "g", "", "Specify the Azure resource group of your AKS cluster")
+	f.StringVarP(&dest, "destination", "d", ".", "Specify the link to the root of repository for Github workflow")
 
 	return cmd
 }

e93592efe5c31585ed3ec4854ec74158813c1413
Erin Borders <48973620+erinborders@users.noreply.github.com>
2022-04-18 19:00:08 -0400

    Allow for loadbalancer or ingress at create (#55)
    * changing service type to loadbalancer and removing ingress manifest
    
    * removing ingress mentions
    
    * adding manifest update logic
    
    * adding type to service manifest
    
    * delete to fix helm integration test failing
    
    * replace annotations test checks for matching length
    
    * removing comments and notes.txt from tests

--- test/check_windows_kustomize.ps1
+++ test/check_windows_kustomize.ps1
@@ -1,8 +1,6 @@
 $filesExist=$true
 $filesExist=$filesExist -and (Test-Path -Path ./base/deployment.yaml -PathType Leaf)
 echo "$file exists: $filesExist"
-$filesExist=$filesExist -and (Test-Path -Path ./base/ingress.yaml -PathType Leaf)
-echo "$file exists: $filesExist"
 $filesExist=$filesExist -and (Test-Path -Path ./base/kustomization.yaml -PathType Leaf)
 echo "$file exists: $filesExist"
 $filesExist=$filesExist -and (Test-Path -Path ./base/service.yaml -PathType Leaf)

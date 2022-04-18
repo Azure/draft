94ded3ecfe82e8398728416a0ef9734f7cefe6bf
eborders <v-eborders@microsoft.com>
2022-04-18 17:45:06 -0400

    removing ingress mentions

--- test/check_windows_helm.ps1
+++ test/check_windows_helm.ps1
@@ -5,8 +5,8 @@ $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/deployment.yaml
 echo "$file exists: $filesExist"
 $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/NOTES.txt -PathType Leaf)
 echo "$file exists: $filesExist"
-$filesExist=$filesExist -and (Test-Path -Path ./charts/templates/ingress.yaml -PathType Leaf)
-echo "$file exists: $filesExist"
+# $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/ingress.yaml -PathType Leaf)
+# echo "$file exists: $filesExist"
 $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/tests/test-connection.yaml -PathType Leaf)
 echo "$file exists: $filesExist"
 $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/service.yaml -PathType Leaf)

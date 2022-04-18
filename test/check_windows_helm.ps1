caf2704119f821fd19aae65e696dd3710e79558e
eborders <v-eborders@microsoft.com>
2022-04-18 18:49:39 -0400

    removing comments and notes.txt from tests

--- test/check_windows_helm.ps1
+++ test/check_windows_helm.ps1
@@ -3,10 +3,6 @@ $filesExist=$filesExist -and (Test-Path -Path ./charts/Chart.yaml -PathType Leaf
 echo "$file exists: $filesExist"
 $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/deployment.yaml -PathType Leaf)
 echo "$file exists: $filesExist"
-$filesExist=$filesExist -and (Test-Path -Path ./charts/templates/NOTES.txt -PathType Leaf)
-echo "$file exists: $filesExist"
-# $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/ingress.yaml -PathType Leaf)
-# echo "$file exists: $filesExist"
 $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/tests/test-connection.yaml -PathType Leaf)
 echo "$file exists: $filesExist"
 $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/service.yaml -PathType Leaf)

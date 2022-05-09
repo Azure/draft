6bb6f26c22e128bcf628bf4f5b8e1c948c2dbad7
eborders <v-eborders@microsoft.com>
2022-05-09 17:42:15 -0400

    increasing setup coverage

--- pkg/providers/providersutils_test.go
+++ pkg/providers/providersutils_test.go
@@ -13,3 +13,14 @@ func TestLoggedInToAz(t *testing.T) {
 func TestLoggedInToGh(t *testing.T) {
 	assert.False(t, IsLoggedInToGh(), "Github is returning logged in even when logged out")
 }
+
+func TestCheckAzCliInstalled(t *testing.T) {
+	var err error 
+	CheckAzCliInstalled()
+
+	assert.Nil(t, err)
+}
+
+func TestHasGhCli(t *testing.T) {
+	assert.True(t, HasGhCli(), "Github CLI is not installed")
+}

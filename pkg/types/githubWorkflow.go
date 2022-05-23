7e21a94dfc52e2b31c69f6b4b7ee9a885f84386c
Karen Chen <karenchen@microsoft.com>
2022-05-23 12:16:28 -0700

    change formatting

--- pkg/types/githubWorkflow.go
+++ pkg/types/githubWorkflow.go
@@ -20,6 +20,6 @@ type push struct {
 type job struct {
 	Permissions map[string]string
 	RunsOn      string `yaml:"runs-on"`
-	Needs		[]string `yaml:"needs,omitempty"`
+	Needs	    []string `yaml:"needs,omitempty"`
 	Steps       []map[string]interface{}
 }

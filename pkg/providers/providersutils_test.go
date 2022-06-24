7e86e1ed0e53a7829787ee6c889a548755a2ee25
Aaron Wislang <aaron.wislang@microsoft.com>
2022-06-24 14:07:43 -0400

    Return errors only vs bool/error, tests, etc.
    IsLoggedInToAz, IsLoggedInToGh, isValidGhRepo return errors only.
    
    Printf -> Debugf
    
    Signed-off-by: Aaron Wislang <aaron.wislang@microsoft.com>

--- pkg/providers/providersutils_test.go
+++ pkg/providers/providersutils_test.go
@@ -7,23 +7,15 @@ import (
 )
 
 func TestLoggedInToAz(t *testing.T) {
-	isLoggedIn, err := IsLoggedInToAz()
-	// In this case we should return false and receive an error.
-	if err == nil {
-		t.Error(err)
-	}
-	assert.False(t, isLoggedIn, "Azure CLI is returning logged in even when logged out")
+	assert.Nil(t, IsLoggedInToAz(), "Azure CLI is returning logged in even when logged out")
 }
 
 func TestLoggedInToGh(t *testing.T) {
-	assert.False(t, IsLoggedInToGh(), "GitHub CLI is returning logged in even when logged out")
+	assert.Nil(t, IsLoggedInToGh(), "GitHub CLI is returning logged in even when logged out")
 }
 
 func TestCheckAzCliInstalled(t *testing.T) {
-	err := CheckAzCliInstalled()
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, CheckAzCliInstalled())
 }
 
 func TestHasGhCli(t *testing.T) {

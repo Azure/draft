6af98507ea5f3d3829213f9ef75423f1461efd16
David Gamero <david340804@gmail.com>
2022-11-03 16:43:12 -0400

    add info command with languages and deploytypes

--- cmd/info.go
+++ cmd/info.go
@@ -1,8 +1,15 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+
+	"github.com/Azure/draft/pkg/deployments"
+	"github.com/Azure/draft/pkg/languages"
+	"github.com/Azure/draft/template"
 )
 
 type Format string
@@ -13,6 +20,12 @@ const (
 
 type infoCmd struct {
 	format string
+	info   *draftInfo
+}
+
+type draftInfo struct {
+	SupportedLanguages       []string `json:"supported_languages"`
+	SupportedDeploymentTypes []string `json:"supported_deployment_types"`
 }
 
 func newInfoCmd() *cobra.Command {
@@ -34,8 +47,21 @@ func newInfoCmd() *cobra.Command {
 	return cmd
 }
 
-func (uc *infoCmd) run() error {
-	log.Println("infoCmd.run() called with format: ", uc.format)
+func (ic *infoCmd) run() error {
+	log.Debugf("getting supported languages")
+	l := languages.CreateLanguagesFromEmbedFS(template.Dockerfiles, "")
+	d := deployments.CreateDeploymentsFromEmbedFS(template.Deployments, "")
+
+	ic.info = &draftInfo{
+		SupportedLanguages:       l.Names(),
+		SupportedDeploymentTypes: d.DeployTypes(),
+	}
+
+	infoText, err := json.MarshalIndent(ic.info, "", "  ")
+	if err != nil {
+		return fmt.Errorf("could not marshal draft info into json: %w", err)
+	}
+	log.Println(string(infoText))
 	return nil
 }
 

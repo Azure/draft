83782174414026e160a44ae99ac6a47838beae85
asgayle <asgayle@microsoft.com>
2023-01-24 16:30:22 -0500

    Returned Windows tests again

--- test/gen_integration.sh
+++ test/gen_integration.sh
@@ -154,4 +154,53 @@ languageVariables:
       - name: Fail if any error
         if: steps.deploy.outcome != 'success'
         run: exit 6" >> ../.github/workflows/integration-linux.yml
+    # create helm workflow
+    echo "
+  $lang-helm-create:
+    runs-on: windows-latest
+    needs: build
+    steps:
+      - uses: actions/checkout@v2
+      - uses: actions/download-artifact@v2
+        with:
+          name: draft-binary
+      - run: mkdir ./langtest
+      - uses: actions/checkout@v2
+        with:
+          repository: $repo
+          path: ./langtest
+      - run: Remove-Item ./langtest/manifests -Recurse -Force -ErrorAction Ignore
+      - run: Remove-Item ./langtest/Dockerfile -ErrorAction Ignore
+      - run: Remove-Item ./langtest/.dockerignore -ErrorAction Ignore
+      - run: ./draft.exe -v create -c ./test/integration/$lang/helm.yaml -d ./langtest/
+      - uses: actions/download-artifact@v2
+        with:
+          name: check_windows_helm
+          path: ./langtest/
+      - run: ./check_windows_helm.ps1
+        working-directory: ./langtest/
+      - uses: actions/upload-artifact@v3
+        with:
+          name: $lang-helm-create
+          path: ./langtest
+  $lang-helm-update:
+    needs: $lang-helm-create
+    runs-on: windows-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: actions/download-artifact@v2
+        with:
+          name: draft-binary
+      - uses: actions/download-artifact@v3
+        with:
+          name: $lang-helm-create
+          path: ./langtest/
+      - run: Remove-Item ./langtest/charts/templates/ingress.yaml -Recurse -Force -ErrorAction Ignore
+      - run: ./draft.exe -v update -d ./langtest/ $ingress_test_args
+      - uses: actions/download-artifact@v2
+        with:
+          name: check_windows_addon_helm
+          path: ./langtest/
+      - run: ./check_windows_addon_helm.ps1
+        working-directory: ./langtest/" >> ../.github/workflows/integration-windows.yml
 done

e5f973e8f61a8c2287acbbdd557e223178a2385d
asgayle <asgayle@microsoft.com>
2023-01-24 15:52:00 -0500

    Ubuntu tests

--- test/gen_integration.sh
+++ test/gen_integration.sh
@@ -34,50 +34,6 @@ jobs:
           name: draft-binary
           path: ./draft
           if-no-files-found: error" > ../.github/workflows/integration-linux.yml
-
-echo "name: draft Windows Integrations
-
-on:
-  push:
-    branches: [ int-tests ]
-  workflow_dispatch:
-
-jobs:
-  build:
-    runs-on: windows-latest
-    steps:
-      - uses: actions/checkout@v2
-      - name: Set up Go
-        uses: actions/setup-go@v2
-        with:
-          go-version: 1.18
-      - name: make
-        run: make
-      - uses: actions/upload-artifact@v2
-        with:
-          name: draft-binary
-          path: ./draft.exe
-          if-no-files-found: error
-      - uses: actions/upload-artifact@v2
-        with:
-          name: check_windows_helm
-          path: ./test/check_windows_helm.ps1
-          if-no-files-found: error
-      - uses: actions/upload-artifact@v2
-        with:
-          name: check_windows_addon_helm
-          path: ./test/check_windows_addon_helm.ps1
-          if-no-files-found: error
-      - uses: actions/upload-artifact@v2
-        with:
-          name: check_windows_kustomize
-          path: ./test/check_windows_kustomize.ps1
-          if-no-files-found: error
-      - uses: actions/upload-artifact@v2
-        with:
-          name: check_windows_addon_kustomize
-          path: ./test/check_windows_addon_kustomize.ps1
-          if-no-files-found: error" > ../.github/workflows/integration-windows.yml
 # read config and add integration test for each language
 cat integration_config.json | jq -c '.[]' | while read -r test;
 do

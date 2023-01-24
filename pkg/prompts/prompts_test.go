1c9b41b267342badaede1e1e7b42122d1565c1e9
David Gamero <david340804@gmail.com>
2023-01-24 12:38:19 -0500

    unit tests for prompting

--- pkg/prompts/prompts_test.go
+++ pkg/prompts/prompts_test.go
@@ -1,6 +1,7 @@
 package prompts
 
 import (
+	"io"
 	"testing"
 
 	"github.com/Azure/draft/pkg/config"
@@ -57,6 +58,67 @@ func TestGetVariableDefaultValue(t *testing.T) {
 		t.Run(tt.testName, func(t *testing.T) {
 			if got := GetVariableDefaultValue(tt.variableName, tt.variableDefaults, tt.inputs); got != tt.want {
 				t.Errorf("GetVariableDefaultValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRunStringPrompt(t *testing.T) {
+	tests := []struct {
+		testName     string
+		prompt       config.BuilderVar
+		userInputs   []string
+		defaultValue string
+		want         string
+		wantErr      bool
+	}{
+		{
+			testName: "basicPrompt",
+			prompt: config.BuilderVar{
+				Name:        "var1",
+				Description: "var1 description",
+			},
+			userInputs:   []string{"value-1\n"},
+			defaultValue: "input",
+			want:         "value-1",
+			wantErr:      false,
+		},
+		{
+			testName: "promptWithDefault",
+			prompt: config.BuilderVar{
+				Name:        "var1",
+				Description: "var1 description",
+			},
+			userInputs:   []string{"\n"},
+			defaultValue: "defaultValue",
+			want:         "defaultValue",
+			wantErr:      false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			inReader, inWriter := io.Pipe()
+
+			go func() {
+				for _, input := range tt.userInputs {
+					_, err := inWriter.Write([]byte(input))
+					if err != nil {
+						t.Errorf("Error writing to inWriter: %v", err)
+					}
+				}
+				err := inWriter.Close()
+				if err != nil {
+					t.Errorf("Error closing inWriter: %v", err)
+				}
+			}()
+			got, err := RunStringPrompt(tt.prompt, tt.defaultValue, inReader, nil)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RunStringPrompt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("RunStringPrompt() = %v, want %v", got, tt.want)
 			}
 		})
 	}

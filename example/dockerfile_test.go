201eff39ad54cb0852938aba01e09747fffddbe9
David Gamero <david340804@gmail.com>
2023-02-28 11:34:47 -0500

    update dockerfile example with usage call

--- example/dockerfile_test.go
+++ example/dockerfile_test.go
@@ -60,3 +60,10 @@ func TestWriteDockerfile(t *testing.T) {
 		})
 	}
 }
+
+func TestWriteDockerfileExample(t *testing.T) {
+	err := WriteDockerfileExample()
+	if err != nil {
+		t.Errorf("WriteDockerfileExample failed: %e", err)
+	}
+}

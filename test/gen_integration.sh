4410f88d45c32cb24cc6695a6e47545475bb1691
David Gamero <david340804@gmail.com>
2023-03-06 17:30:29 -0500

    add curl on manifest create

--- test/gen_integration.sh
+++ test/gen_integration.sh
@@ -404,7 +404,7 @@ languageVariables:
         id: deploy
       - name: Wait for rollout
         run: |
-          kubectl rollout status deployment testapp --timeout=30s
+          kubectl rollout status deployment testapp --timeout=120s
       - name: Check default namespace
         run: |
           kubectl get po

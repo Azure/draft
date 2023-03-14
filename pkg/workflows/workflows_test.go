bf307745e3dac7f4260981493a208138065bd5fa
Suneha Bose <sunehabose@microsoft.com>
2023-03-14 11:54:58 -0700

    updated workflows test

--- pkg/workflows/workflows_test.go
+++ pkg/workflows/workflows_test.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Azure/draft/template"
+	"github.com/Azure/draft/pkg/templatewriter/writers"
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -33,69 +33,17 @@ func createTempManifest(path string) (string, error) {
 	return file.Name(), nil
 }
 
-func TestWorkflowEmbed(t *testing.T) {
-	workflow := &workflowType{
-		deployPath:         "/charts",
-		workflowFileSuffix: "-helm",
-	}
-
-	assert.NotEmptyf(t, getWorkflowFile(workflow, template.Workflows), "workflow should be fetched from the embeded file system")
-}
-
-func TestWorkflowReplace(t *testing.T) {
-	config := &WorkflowConfig{
-		AcrName:           "test",
-		AksClusterName:    "test",
-		ContainerName:     "test",
-		ResourceGroupName: "test",
-		BranchName:        "test",
-
-		ChartsOverridePath: "testOverride",
-		KustomizePath:      "testKustomize",
-	}
-
-	ghw := &GitHubWorkflow{}
-	ghw.On.Push.Branches = []string{"branch"}
-	replaceWorkflowVars("", config, ghw)
-	assert.NotNil(t, ghw.Env, "check that replace will update a ghw's environment")
-
-	workflow, ok := deployNameToWorkflow["manifests"]
-	assert.True(t, ok)
-
-	ghw = getWorkflowFile(workflow, template.Workflows)
-	origLen := len(ghw.Jobs["build"].Steps)
-	replaceWorkflowVars("manifests", config, ghw)
-	assert.Equal(t, origLen, len(ghw.Jobs["build"].Steps), "check step is deleted")
-
-	workflow, ok = deployNameToWorkflow["helm"]
-	assert.True(t, ok)
-
-	ghw = getWorkflowFile(workflow, template.Workflows)
-	replaceWorkflowVars("helm", config, ghw)
-	assert.Equal(t, "testOverride", ghw.Env["CHART_OVERRIDE_PATH"], "check helm envs are replaced")
-
-	workflow, ok = deployNameToWorkflow["kustomize"]
-	assert.True(t, ok)
-
-	ghw = getWorkflowFile(workflow, template.Workflows)
-	replaceWorkflowVars("kustomize", config, ghw)
-	assert.Equal(t, "testKustomize", ghw.Env["KUSTOMIZE_PATH"], "check kustomize envs are replaces")
-}
-
 func TestUpdateProductionDeployments(t *testing.T) {
-	config := &WorkflowConfig{
-		AcrName:           "test",
-		ContainerName:     "test",
-		ResourceGroupName: "test",
-	}
-	assert.Nil(t, updateProductionDeployments("", ".", config))
+	flagValuesMap := map[string]string{"AZURECONTAINERREGISTRY": "testRegistry", "CONTAINERNAME": "testContainer"}
+	testTemplateWriter := &writers.LocalFSWriter{}
+	assert.Nil(t, updateProductionDeployments("", ".", flagValuesMap, testTemplateWriter))
 
 	helmFileName, _ := createTempManifest("../../test/templates/helm_prod_values.yaml")
 	deploymentFileName, _ := createTempManifest("../../test/templates/deployment.yaml")
 	defer os.Remove(helmFileName)
 	defer os.Remove(deploymentFileName)
 
-	assert.Nil(t, setHelmContainerImage(helmFileName, "testImage"))
+	assert.Nil(t, setHelmContainerImage(helmFileName, "testImage", testTemplateWriter))
 
 	helmDeploy := &HelmProductionYaml{}
 	assert.Nil(t, helmDeploy.LoadFromFile(helmFileName))

239b21d2bd4dc3d7fc2524d6114d355526d6da5e
Suneha Bose <sunehabose@microsoft.com>
2023-03-14 14:49:25 -0700

    log for debugging

--- pkg/osutil/osutil.go
+++ pkg/osutil/osutil.go
@@ -97,7 +97,7 @@ func CopyDir(
 
 		srcPath := path.Join(src, f.Name())
 		destPath := path.Join(dest, f.Name())
-		log.Debugf("Source path: %s", srcPath+" Dest path: %s", destPath)
+		log.Debugf("Source path: %s Dest path: %s", srcPath, destPath)
 
 		if f.IsDir() {
 			if err = templateWriter.EnsureDirectory(destPath); err != nil {

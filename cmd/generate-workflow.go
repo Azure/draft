6c1eaee76351149822932bf22d25a05dfce084f0
Suneha Bose <sunehabose@microsoft.com>
2023-03-14 11:26:52 -0700

    refactored generate-workflow

--- cmd/generate-workflow.go
+++ cmd/generate-workflow.go
@@ -1,28 +1,40 @@
 package cmd
 
 import (
+	"github.com/Azure/draft/pkg/config"
+	"github.com/Azure/draft/pkg/templatewriter"
+	"github.com/Azure/draft/pkg/templatewriter/writers"
 	"github.com/Azure/draft/pkg/workflows"
-	"github.com/Azure/draft/template"
+
 	"github.com/spf13/cobra"
 
 	log "github.com/sirupsen/logrus"
 )
 
+type generateWorkflowCmd struct {
+	workflowConfig workflows.WorkflowConfig
+	dest           string
+	deployType     string
+	flagVariables  []string
+	templateWriter templatewriter.TemplateWriter
+}
+
+var draftWorkflowConfig *config.DraftConfig
+var setFlags []string
+
 func newGenerateWorkflowCmd() *cobra.Command {
 
-	workflowConfig := &workflows.WorkflowConfig{}
-	dest := ""
+	gwCmd := &generateWorkflowCmd{}
+	gwCmd.dest = ""
 	var cmd = &cobra.Command{
 		Use:   "generate-workflow [flags]",
 		Short: "Generates a Github workflow for automatic build and deploy to AKS",
 		Long: `This command will generate a Github workflow to build and deploy an application containerized 
 with draft on AKS. This command assumes the 'setup-gh' command has been run properly.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			workflowConfig.ValidateAndFillConfig()
-
+			flagValuesMap := gwCmd.workflowConfig.SetFlagValuesToMap()
 			log.Info("--> Generating Github workflow")
-
-			if err := workflows.CreateWorkflows(dest, workflowConfig, template.Workflows); err != nil {
+			if err := workflows.CreateWorkflows(gwCmd.dest, gwCmd.deployType, &gwCmd.workflowConfig, gwCmd.flagVariables, gwCmd.templateWriter, flagValuesMap); err != nil {
 				return err
 			}
 
@@ -33,13 +45,15 @@ with draft on AKS. This command assumes the 'setup-gh' command has been run prop
 	}
 
 	f := cmd.Flags()
-	f.StringVarP(&workflowConfig.AksClusterName, "cluster-name", "c", "", "specify the AKS cluster name")
-	f.StringVarP(&workflowConfig.AcrName, "registry-name", "r", "", "specify the Azure container registry name")
-	f.StringVar(&workflowConfig.ContainerName, "container-name", "", "specify the container image name")
-	f.StringVarP(&workflowConfig.ResourceGroupName, "resource-group", "g", "", "Specify the Azure resource group of your AKS cluster")
-	f.StringVarP(&dest, "destination", "d", ".", "specify the path to the project directory")
-	f.StringVarP(&workflowConfig.BranchName, "branch", "b", "", "specify the Github branch to automatically deploy from")
-
+	f.StringVarP(&gwCmd.workflowConfig.AksClusterName, "cluster-name", "c", "", "specify the AKS cluster name")
+	f.StringVarP(&gwCmd.workflowConfig.AcrName, "registry-name", "r", "", "specify the Azure container registry name")
+	f.StringVar(&gwCmd.workflowConfig.ContainerName, "container-name", "", "specify the container image name")
+	f.StringVarP(&gwCmd.workflowConfig.ResourceGroupName, "resource-group", "g", "", "Specify the Azure resource group of your AKS cluster")
+	f.StringVarP(&gwCmd.dest, "destination", "d", ".", "specify the path to the project directory")
+	f.StringVarP(&gwCmd.workflowConfig.BranchName, "branch", "b", "", "specify the Github branch to automatically deploy from")
+	f.StringVar(&gwCmd.deployType, "deploy-type", "", "specify the type of deployment")
+	f.StringArrayVarP(&gwCmd.flagVariables, "variable", "", []string{}, "pass additional variables")
+	gwCmd.templateWriter = &writers.LocalFSWriter{}
 	return cmd
 }
 

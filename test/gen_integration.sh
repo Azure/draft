1519edc6f2b7f036d96eed9b4a48f062b5f8c66a
asgayle <asgayle@microsoft.com>
2023-03-23 16:40:32 -0400

    Fixed typo in build-context-path flag

--- test/gen_integration.sh
+++ test/gen_integration.sh
@@ -300,7 +300,7 @@ languageVariables:
           echo 'Curling service IP'
           curl -m 3 \$SERVICEIP:$serviceport
           kill \$tunnelPID
-      - run: ./draft -b main -v generate-workflow -d ./langtest/ -c someAksCluster -r someRegistry -g someResourceGroup --container-name someContainer -build-context-path .
+      - run: ./draft -b main -v generate-workflow -d ./langtest/ -c someAksCluster -r someRegistry -g someResourceGroup --container-name someContainer --build-context-path .
       - name: Execute dry run for update command
         run: |
           mkdir -p test/temp
@@ -432,7 +432,7 @@ languageVariables:
           echo 'Curling service IP'
           curl -m 3 \$SERVICEIP:$serviceport
           kill \$tunnelPID
-      - run: ./draft -v generate-workflow -b main -d ./langtest/ -c someAksCluster -r someRegistry -g someResourceGroup --container-name someContainer -build-context-path .
+      - run: ./draft -v generate-workflow -b main -d ./langtest/ -c someAksCluster -r someRegistry -g someResourceGroup --container-name someContainer --build-context-path .
       - name: Execute dry run for update command
         run: |
           mkdir -p test/temp
@@ -556,7 +556,7 @@ languageVariables:
           echo 'Curling service IP'
           curl -m 3 \$SERVICEIP:$serviceport
           kill \$tunnelPID
-      - run: ./draft -v generate-workflow -d ./langtest/ -b main -c someAksCluster -r localhost -g someResourceGroup --container-name testapp -build-context-path .
+      - run: ./draft -v generate-workflow -d ./langtest/ -b main -c someAksCluster -r localhost -g someResourceGroup --container-name testapp --build-context-path .
       - uses: actions/upload-artifact@v3
         with:
           name: $lang-manifests-create

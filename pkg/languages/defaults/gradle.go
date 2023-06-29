89bb0666cac09baf936994aebc975fad7dcde387
Hariharan Subramanian <hsubramanian@microsoft.com>
2023-06-29 22:45:49 +0000

    review changes
    Signed-off-by: Hariharan Subramanian <hsubramanian@microsoft.com>

--- pkg/languages/defaults/gradle.go
+++ pkg/languages/defaults/gradle.go
@@ -23,6 +23,8 @@ func (*GradleExtractor) MatchesLanguage(lowerlang string) bool {
 
 // ReadDefaults implements reporeader.VariableExtractor
 func (*GradleExtractor) ReadDefaults(r reporeader.RepoReader) (map[string]string, error) {
+	separatorsSet := createSeparatorsSet()
+	cutSet := createCutSet()
 	extractedValues := make(map[string]string)
 	files, err := r.FindFiles(".", []string{"*.gradle"}, 2)
 	if err != nil {
@@ -31,29 +33,29 @@ func (*GradleExtractor) ReadDefaults(r reporeader.RepoReader) (map[string]string
 	if len(files) > 0 {
 		f, err := r.ReadFile(files[0])
 		if err != nil {
-			log.Warn("Unable to read build.gradle, skipping detection")
+			log.Warn("Unable to read gradle file, skipping detection")
 			return nil, nil
 		}
 		content := string(f)
 		// this separator is used to split the line from build.gradle ex: sourceCompatibility = '1.8'
 		// output will be ['sourceCompatibility', '1.8'] or ["sourceCompatibility", "1.8"]
-		separator := func(c rune) bool {
-			return c == ' ' || c == '=' || c == '\n' || c == '\r' || c == '\t' || c == '{' || c == '}' || c == '[' || c == ']' || c == '-'
+		separatorFunc := func(c rune) bool {
+			return separatorsSet.Contains(c)
 		}
 		// this func takes care of removing the single or double quotes from split array output
-		cutset := func(c rune) bool { return c == '\'' || c == '"' }
+		cutset := func(c rune) bool { return cutSet.Contains(c) }
 		if strings.Contains(content, "sourceCompatibility") || strings.Contains(content, "targetCompatibility") || strings.Contains(content, "server.port") {
-			stringAfterSplit := strings.FieldsFunc(content, separator)
-			for i := 0; i < len(stringAfterSplit); i++ {
-				if stringAfterSplit[i] == "sourceCompatibility" {
+			stringAfterSplit := strings.FieldsFunc(content, separatorFunc)
+			for i, s := range stringAfterSplit {
+				if s == "sourceCompatibility" && i+1 < len(stringAfterSplit) {
 					detectedVersion := strings.TrimFunc(stringAfterSplit[i+1], cutset)
 					detectedVersion = detectedVersion + "-jre"
 					extractedValues["VERSION"] = detectedVersion
-				} else if stringAfterSplit[i] == "targetCompatibility" {
+				} else if s == "targetCompatibility" && i+1 < len(stringAfterSplit) {
 					detectedBuilderVersion := strings.TrimFunc(stringAfterSplit[i+1], cutset)
 					detectedBuilderVersion = "jdk" + detectedBuilderVersion
 					extractedValues["BUILDERVERSION"] = detectedBuilderVersion
-				} else if stringAfterSplit[i] == "server.port" {
+				} else if s == "server.port" && i+1 < len(stringAfterSplit) {
 					detectedPort := strings.TrimFunc(stringAfterSplit[i+1], cutset)
 					extractedValues["PORT"] = detectedPort
 				}
@@ -62,6 +64,44 @@ func (*GradleExtractor) ReadDefaults(r reporeader.RepoReader) (map[string]string
 	}
 
 	return extractedValues, nil
+}
+
+func createSeparatorsSet() Set {
+	separatorsSet := NewSet()
+	separatorsSet.Add(' ')
+	separatorsSet.Add('=')
+	separatorsSet.Add('\n')
+	separatorsSet.Add('\r')
+	separatorsSet.Add('\t')
+	separatorsSet.Add('{')
+	separatorsSet.Add('}')
+	separatorsSet.Add('[')
+	separatorsSet.Add(']')
+	separatorsSet.Add('-')
+	separatorsSet.Add(':')
+	return separatorsSet
+}
+
+func createCutSet() Set {
+	cutSet := NewSet()
+	cutSet.Add('\'')
+	cutSet.Add('"')
+	return cutSet
+}
+
+type Set map[interface{}]struct{}
+
+func NewSet() Set {
+	return make(Set)
+}
+
+func (s Set) Add(item interface{}) {
+	s[item] = struct{}{}
+}
+
+func (s Set) Contains(item interface{}) bool {
+	_, ok := s[item]
+	return ok
 }
 
 var _ reporeader.VariableExtractor = &GradleExtractor{}

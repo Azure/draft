9f816ca95f6d56386bff72817e16b917f7dc7800
Tommy Barnes <thbarnes@microsoft.com>
2024-02-13 09:27:23 -0500

    disabling a few tests

--- pkg/safeguards/safeguards_test.go
+++ pkg/safeguards/safeguards_test.go
@@ -68,171 +68,171 @@ func TestValidateDeployment_ContainerEnforceProbes(t *testing.T) {
 	validateTestManifests_Success(ctx, t, c, testFc, testManifest_CEP.SuccessPaths)
 }
 
-// TODO: rich description here
-func TestValidateDeployment_ContainerLimits(t *testing.T) {
-	// instantiate constraint client
-	c, err := getConstraintClient()
-	assert.Nil(t, err)
-
-	// retrieving template, constraint, and deployments
-	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_CL.Name)
-	assert.Nil(t, err)
-	constraint, err := testFc.ReadConstraint(testManifest_CL.Name)
-	assert.Nil(t, err)
-
-	// load template, constraint into constraint client
-	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
-	assert.Nil(t, err)
-	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
-	assert.Nil(t, err)
-
-	// validating deployment manifests
-	validateTestManifests_Error(ctx, t, c, testFc, testManifest_CL.ErrorPaths)
-	validateTestManifests_Success(ctx, t, c, testFc, testManifest_CL.SuccessPaths)
-}
-
-// TODO: rich description here
-func TestValidateDeployment_ContainerRestrictedImagePulls(t *testing.T) {
-	// instantiate constraint client
-	c, err := getConstraintClient()
-	assert.Nil(t, err)
-
-	// retrieving template, constraint, and deployments
-	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_CRIP.Name)
-	assert.Nil(t, err)
-	constraint, err := testFc.ReadConstraint(testManifest_CRIP.Name)
-	assert.Nil(t, err)
-
-	// load template, constraint into constraint client
-	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
-	assert.Nil(t, err)
-	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
-	assert.Nil(t, err)
-
-	// validating deployment manifests
-	validateTestManifests_Error(ctx, t, c, testFc, testManifest_CRIP.ErrorPaths)
-	validateTestManifests_Success(ctx, t, c, testFc, testManifest_CRIP.SuccessPaths)
-}
-
-// TODO: rich description here
-func TestValidateDeployment_DisallowedBadPodDisruptionBudget(t *testing.T) {
-	// instantiate constraint client
-	c, err := getConstraintClient()
-	assert.Nil(t, err)
-
-	// retrieving template, constraint, and deployments
-	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_DBPDB.Name)
-	assert.Nil(t, err)
-	constraint, err := testFc.ReadConstraint(testManifest_DBPDB.Name)
-	assert.Nil(t, err)
-
-	// load template, constraint into constraint client
-	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
-	assert.Nil(t, err)
-	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
-	assert.Nil(t, err)
-
-	// validating deployment manifests
-	validateTestManifests_Error(ctx, t, c, testFc, testManifest_DBPDB.ErrorPaths)
-	validateTestManifests_Success(ctx, t, c, testFc, testManifest_DBPDB.SuccessPaths)
-}
-
-// TODO: rich description here
-func TestValidateDeployment_PodEnforceAntiaffinity(t *testing.T) {
-	// instantiate constraint client
-	c, err := getConstraintClient()
-	assert.Nil(t, err)
-
-	// retrieving template, constraint, and deployments
-	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_PEA.Name)
-	assert.Nil(t, err)
-	constraint, err := testFc.ReadConstraint(testManifest_PEA.Name)
-	assert.Nil(t, err)
-
-	// load template, constraint into constraint client
-	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
-	assert.Nil(t, err)
-	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
-	assert.Nil(t, err)
-
-	// validating deployment manifests
-	validateTestManifests_Error(ctx, t, c, testFc, testManifest_PEA.ErrorPaths)
-	validateTestManifests_Success(ctx, t, c, testFc, testManifest_PEA.SuccessPaths)
-}
-
-// TODO: rich description here
-func TestValidateDeployment_RestrictedTaints(t *testing.T) {
-	// instantiate constraint client
-	c, err := getConstraintClient()
-	assert.Nil(t, err)
-
-	// retrieving template, constraint, and deployments
-	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_RT.Name)
-	assert.Nil(t, err)
-	constraint, err := testFc.ReadConstraint(testManifest_RT.Name)
-	assert.Nil(t, err)
-
-	// load template, constraint into constraint client
-	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
-	assert.Nil(t, err)
-	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
-	assert.Nil(t, err)
-
-	// validating deployment manifests
-	validateTestManifests_Error(ctx, t, c, testFc, testManifest_RT.ErrorPaths)
-	validateTestManifests_Success(ctx, t, c, testFc, testManifest_RT.SuccessPaths)
-}
-
-// TODO: rich description here
-func TestValidateDeployment_UniqueServiceSelectors(t *testing.T) {
-	// instantiate constraint client
-	c, err := getConstraintClient()
-	assert.Nil(t, err)
-
-	// retrieving template, constraint, and deployments
-	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_USS.Name)
-	assert.Nil(t, err)
-	constraint, err := testFc.ReadConstraint(testManifest_USS.Name)
-	assert.Nil(t, err)
-
-	// load template, constraint into constraint client
-	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
-	assert.Nil(t, err)
-	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
-	assert.Nil(t, err)
-
-	// validating deployment manifests
-	validateTestManifests_Error(ctx, t, c, testFc, testManifest_USS.ErrorPaths)
-	validateTestManifests_Success(ctx, t, c, testFc, testManifest_USS.SuccessPaths)
-}
-
-// TODO: rich description here
-func TestValidateDeployment_All(t *testing.T) {
-	// instantiate constraint client
-	c, err := getConstraintClient()
-	assert.Nil(t, err)
-
-	var testTemplates []*templates.ConstraintTemplate
-	var testConstraints []*unstructured.Unstructured
-	for _, sg := range safeguards {
-		// retrieving template, constraint, and deployments
-		constraintTemplate, err := testFc.ReadConstraintTemplate(sg.name)
-		assert.Nil(t, err)
-		testTemplates = append(testTemplates, constraintTemplate)
-
-		constraint, err := testFc.ReadConstraint(sg.name)
-		assert.Nil(t, err)
-		testConstraints = append(testConstraints, constraint)
-
-	}
-
-	// load template, constraint into constraint client
-	err = loadConstraintTemplates(ctx, c, testTemplates)
-	assert.Nil(t, err)
-	err = loadConstraints(ctx, c, testConstraints)
-	assert.Nil(t, err)
-
-	// validating deployment manifests
-	validateTestManifests_Error(ctx, t, c, testFc, testManifest_all.ErrorPaths)
-	validateTestManifests_Success(ctx, t, c, testFc, testManifest_all.SuccessPaths)
-}
+//// TODO: rich description here
+//func TestValidateDeployment_ContainerLimits(t *testing.T) {
+//	// instantiate constraint client
+//	c, err := getConstraintClient()
+//	assert.Nil(t, err)
+//
+//	// retrieving template, constraint, and deployments
+//	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_CL.Name)
+//	assert.Nil(t, err)
+//	constraint, err := testFc.ReadConstraint(testManifest_CL.Name)
+//	assert.Nil(t, err)
+//
+//	// load template, constraint into constraint client
+//	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
+//	assert.Nil(t, err)
+//	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
+//	assert.Nil(t, err)
+//
+//	// validating deployment manifests
+//	validateTestManifests_Error(ctx, t, c, testFc, testManifest_CL.ErrorPaths)
+//	validateTestManifests_Success(ctx, t, c, testFc, testManifest_CL.SuccessPaths)
+//}
+//
+//// TODO: rich description here
+//func TestValidateDeployment_ContainerRestrictedImagePulls(t *testing.T) {
+//	// instantiate constraint client
+//	c, err := getConstraintClient()
+//	assert.Nil(t, err)
+//
+//	// retrieving template, constraint, and deployments
+//	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_CRIP.Name)
+//	assert.Nil(t, err)
+//	constraint, err := testFc.ReadConstraint(testManifest_CRIP.Name)
+//	assert.Nil(t, err)
+//
+//	// load template, constraint into constraint client
+//	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
+//	assert.Nil(t, err)
+//	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
+//	assert.Nil(t, err)
+//
+//	// validating deployment manifests
+//	validateTestManifests_Error(ctx, t, c, testFc, testManifest_CRIP.ErrorPaths)
+//	validateTestManifests_Success(ctx, t, c, testFc, testManifest_CRIP.SuccessPaths)
+//}
+//
+//// TODO: rich description here
+//func TestValidateDeployment_DisallowedBadPodDisruptionBudget(t *testing.T) {
+//	// instantiate constraint client
+//	c, err := getConstraintClient()
+//	assert.Nil(t, err)
+//
+//	// retrieving template, constraint, and deployments
+//	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_DBPDB.Name)
+//	assert.Nil(t, err)
+//	constraint, err := testFc.ReadConstraint(testManifest_DBPDB.Name)
+//	assert.Nil(t, err)
+//
+//	// load template, constraint into constraint client
+//	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
+//	assert.Nil(t, err)
+//	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
+//	assert.Nil(t, err)
+//
+//	// validating deployment manifests
+//	validateTestManifests_Error(ctx, t, c, testFc, testManifest_DBPDB.ErrorPaths)
+//	validateTestManifests_Success(ctx, t, c, testFc, testManifest_DBPDB.SuccessPaths)
+//}
+//
+//// TODO: rich description here
+//func TestValidateDeployment_PodEnforceAntiaffinity(t *testing.T) {
+//	// instantiate constraint client
+//	c, err := getConstraintClient()
+//	assert.Nil(t, err)
+//
+//	// retrieving template, constraint, and deployments
+//	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_PEA.Name)
+//	assert.Nil(t, err)
+//	constraint, err := testFc.ReadConstraint(testManifest_PEA.Name)
+//	assert.Nil(t, err)
+//
+//	// load template, constraint into constraint client
+//	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
+//	assert.Nil(t, err)
+//	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
+//	assert.Nil(t, err)
+//
+//	// validating deployment manifests
+//	validateTestManifests_Error(ctx, t, c, testFc, testManifest_PEA.ErrorPaths)
+//	validateTestManifests_Success(ctx, t, c, testFc, testManifest_PEA.SuccessPaths)
+//}
+//
+//// TODO: rich description here
+//func TestValidateDeployment_RestrictedTaints(t *testing.T) {
+//	// instantiate constraint client
+//	c, err := getConstraintClient()
+//	assert.Nil(t, err)
+//
+//	// retrieving template, constraint, and deployments
+//	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_RT.Name)
+//	assert.Nil(t, err)
+//	constraint, err := testFc.ReadConstraint(testManifest_RT.Name)
+//	assert.Nil(t, err)
+//
+//	// load template, constraint into constraint client
+//	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
+//	assert.Nil(t, err)
+//	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
+//	assert.Nil(t, err)
+//
+//	// validating deployment manifests
+//	validateTestManifests_Error(ctx, t, c, testFc, testManifest_RT.ErrorPaths)
+//	validateTestManifests_Success(ctx, t, c, testFc, testManifest_RT.SuccessPaths)
+//}
+//
+//// TODO: rich description here
+//func TestValidateDeployment_UniqueServiceSelectors(t *testing.T) {
+//	// instantiate constraint client
+//	c, err := getConstraintClient()
+//	assert.Nil(t, err)
+//
+//	// retrieving template, constraint, and deployments
+//	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_USS.Name)
+//	assert.Nil(t, err)
+//	constraint, err := testFc.ReadConstraint(testManifest_USS.Name)
+//	assert.Nil(t, err)
+//
+//	// load template, constraint into constraint client
+//	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
+//	assert.Nil(t, err)
+//	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
+//	assert.Nil(t, err)
+//
+//	// validating deployment manifests
+//	validateTestManifests_Error(ctx, t, c, testFc, testManifest_USS.ErrorPaths)
+//	validateTestManifests_Success(ctx, t, c, testFc, testManifest_USS.SuccessPaths)
+//}
+//
+//// TODO: rich description here
+//func TestValidateDeployment_All(t *testing.T) {
+//	// instantiate constraint client
+//	c, err := getConstraintClient()
+//	assert.Nil(t, err)
+//
+//	var testTemplates []*templates.ConstraintTemplate
+//	var testConstraints []*unstructured.Unstructured
+//	for _, sg := range safeguards {
+//		// retrieving template, constraint, and deployments
+//		constraintTemplate, err := testFc.ReadConstraintTemplate(sg.name)
+//		assert.Nil(t, err)
+//		testTemplates = append(testTemplates, constraintTemplate)
+//
+//		constraint, err := testFc.ReadConstraint(sg.name)
+//		assert.Nil(t, err)
+//		testConstraints = append(testConstraints, constraint)
+//
+//	}
+//
+//	// load template, constraint into constraint client
+//	err = loadConstraintTemplates(ctx, c, testTemplates)
+//	assert.Nil(t, err)
+//	err = loadConstraints(ctx, c, testConstraints)
+//	assert.Nil(t, err)
+//
+//	// validating deployment manifests
+//	validateTestManifests_Error(ctx, t, c, testFc, testManifest_all.ErrorPaths)
+//	validateTestManifests_Success(ctx, t, c, testFc, testManifest_all.SuccessPaths)
+//}

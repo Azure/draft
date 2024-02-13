18ee9a8ef70d9d1c2c17b1dab03ed6ff3ac9b0bb
Tommy Barnes <thbarnes@microsoft.com>
2024-02-13 10:06:49 -0500

    Enabled CRIP UT

--- pkg/safeguards/safeguards_test.go
+++ pkg/safeguards/safeguards_test.go
@@ -68,52 +68,53 @@ func TestValidateDeployment_ContainerEnforceProbes(t *testing.T) {
 	validateTestManifests_Success(ctx, t, c, testFc, testManifest_CEP.SuccessPaths)
 }
 
-//// TODO: rich description here
-//func TestValidateDeployment_ContainerLimits(t *testing.T) {
-//	// instantiate constraint client
-//	c, err := getConstraintClient()
-//	assert.Nil(t, err)
+// // TODO: rich description here
 //
-//	// retrieving template, constraint, and deployments
-//	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_CL.Name)
-//	assert.Nil(t, err)
-//	constraint, err := testFc.ReadConstraint(testManifest_CL.Name)
-//	assert.Nil(t, err)
+//	func TestValidateDeployment_ContainerLimits(t *testing.T) {
+//		// instantiate constraint client
+//		c, err := getConstraintClient()
+//		assert.Nil(t, err)
 //
-//	// load template, constraint into constraint client
-//	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
-//	assert.Nil(t, err)
-//	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
-//	assert.Nil(t, err)
+//		// retrieving template, constraint, and deployments
+//		constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_CL.Name)
+//		assert.Nil(t, err)
+//		constraint, err := testFc.ReadConstraint(testManifest_CL.Name)
+//		assert.Nil(t, err)
 //
-//	// validating deployment manifests
-//	validateTestManifests_Error(ctx, t, c, testFc, testManifest_CL.ErrorPaths)
-//	validateTestManifests_Success(ctx, t, c, testFc, testManifest_CL.SuccessPaths)
-//}
+//		// load template, constraint into constraint client
+//		err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
+//		assert.Nil(t, err)
+//		err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
+//		assert.Nil(t, err)
 //
-//// TODO: rich description here
-//func TestValidateDeployment_ContainerRestrictedImagePulls(t *testing.T) {
-//	// instantiate constraint client
-//	c, err := getConstraintClient()
-//	assert.Nil(t, err)
+//		// validating deployment manifests
+//		validateTestManifests_Error(ctx, t, c, testFc, testManifest_CL.ErrorPaths)
+//		validateTestManifests_Success(ctx, t, c, testFc, testManifest_CL.SuccessPaths)
+//	}
 //
-//	// retrieving template, constraint, and deployments
-//	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_CRIP.Name)
-//	assert.Nil(t, err)
-//	constraint, err := testFc.ReadConstraint(testManifest_CRIP.Name)
-//	assert.Nil(t, err)
-//
-//	// load template, constraint into constraint client
-//	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
-//	assert.Nil(t, err)
-//	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
-//	assert.Nil(t, err)
-//
-//	// validating deployment manifests
-//	validateTestManifests_Error(ctx, t, c, testFc, testManifest_CRIP.ErrorPaths)
-//	validateTestManifests_Success(ctx, t, c, testFc, testManifest_CRIP.SuccessPaths)
-//}
-//
+// TODO: rich description here
+func TestValidateDeployment_ContainerRestrictedImagePulls(t *testing.T) {
+	// instantiate constraint client
+	c, err := getConstraintClient()
+	assert.Nil(t, err)
+
+	// retrieving template, constraint, and deployments
+	constraintTemplate, err := testFc.ReadConstraintTemplate(testManifest_CRIP.Name)
+	assert.Nil(t, err)
+	constraint, err := testFc.ReadConstraint(testManifest_CRIP.Name)
+	assert.Nil(t, err)
+
+	// load template, constraint into constraint client
+	err = loadConstraintTemplates(ctx, c, []*templates.ConstraintTemplate{constraintTemplate})
+	assert.Nil(t, err)
+	err = loadConstraints(ctx, c, []*unstructured.Unstructured{constraint})
+	assert.Nil(t, err)
+
+	// validating deployment manifests
+	validateTestManifests_Error(ctx, t, c, testFc, testManifest_CRIP.ErrorPaths)
+	validateTestManifests_Success(ctx, t, c, testFc, testManifest_CRIP.SuccessPaths)
+}
+
 //// TODO: rich description here
 //func TestValidateDeployment_DisallowedBadPodDisruptionBudget(t *testing.T) {
 //	// instantiate constraint client

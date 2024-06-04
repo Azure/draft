021ceaa09452a385a847376b1c0e338dcaf37c72
Vidya Reddy <59590642+Vidya2606@users.noreply.github.com>
2024-06-04 13:57:42 -0700

    Reverting the graphsdk to azcli (#295)

--- pkg/providers/az-client.go
+++ pkg/providers/az-client.go
@@ -1,55 +1,15 @@
 package providers
 
 import (
-	"context"
-	"errors"
-	"fmt"
-
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/authorization/armauthorization"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/subscription/armsubscription"
-
-	msgraph "github.com/microsoftgraph/msgraph-sdk-go"
 )
 
 type AzClient struct {
-	AzTenantClient   azTenantClient
-	GraphClient      GraphClient
-	RoleAssignClient RoleAssignClient
+	AzTenantClient azTenantClient
 }
 
 //go:generate mockgen -source=./az-client.go -destination=./mock/az-client.go .
 type azTenantClient interface {
 	NewListPager(options *armsubscription.TenantsClientListOptions) *runtime.Pager[armsubscription.TenantsClientListResponse]
 }
-
-// GraphServiceClient implements the GraphClient interface.
-type GraphServiceClient struct {
-	Client *msgraph.GraphServiceClient
-}
-
-type GraphClient interface {
-	GetApplicationObjectId(ctx context.Context, appId string) (string, error)
-}
-
-var _ GraphClient = &GraphServiceClient{}
-
-func (g *GraphServiceClient) GetApplicationObjectId(ctx context.Context, appId string) (string, error) {
-	req := g.Client.Applications().ByApplicationId(appId)
-
-	app, err := req.Get(ctx, nil)
-	if err != nil {
-		return "", fmt.Errorf("getting application details: %w", err)
-	}
-	appObjectId := app.GetAppId()
-	if appObjectId == nil || *appObjectId == "" {
-		return "", errors.New("application object ID is empty")
-	}
-	return *appObjectId, nil
-}
-
-type RoleAssignClient interface {
-	CreateByID(ctx context.Context, roleAssignmentID string, parameters armauthorization.RoleAssignmentCreateParameters, options *armauthorization.RoleAssignmentsClientCreateByIDOptions) (armauthorization.RoleAssignmentsClientCreateByIDResponse, error)
-}
-
-var _ RoleAssignClient = &armauthorization.RoleAssignmentsClient{}

96f500a0f9fcc12c6e385713b464d1dfde03e445
Mauricio Ferrari <t-mferrari@microsoft.com>
2024-06-27 23:37:04 -0400

    Workflow Template Enhancements (#324)
    Co-authored-by: Vidya Reddy <59590642+Vidya2606@users.noreply.github.com>

--- pkg/prompts/prompts.go
+++ pkg/prompts/prompts.go
@@ -20,18 +20,18 @@ const defaultAppName = "my-app"
 // Function to get current directory name
 var getCurrentDirNameFunc = getCurrentDirName
 
-func RunPromptsFromConfig(config *config.DraftConfig) (map[string]string, error) {
-	return RunPromptsFromConfigWithSkips(config, []string{})
+func RunPromptsFromConfig(draftConfig *config.DraftConfig) (map[string]string, error) {
+	return RunPromptsFromConfigWithSkips(draftConfig, []string{})
 }
 
-func RunPromptsFromConfigWithSkips(config *config.DraftConfig, varsToSkip []string) (map[string]string, error) {
-	return RunPromptsFromConfigWithSkipsIO(config, varsToSkip, nil, nil)
+func RunPromptsFromConfigWithSkips(draftConfig *config.DraftConfig, varsToSkip []string) (map[string]string, error) {
+	return RunPromptsFromConfigWithSkipsIO(draftConfig, varsToSkip, nil, nil)
 }
 
-// RunPromptsFromConfigWithSkipsIO runs the prompts for the given config
+// RunPromptsFromConfigWithSkipsIO runs the prompts for the given draftConfig
 // skipping any variables in varsToSkip or where the BuilderVar.IsPromptDisabled is true.
 // If Stdin or Stdout are nil, the default values will be used.
-func RunPromptsFromConfigWithSkipsIO(config *config.DraftConfig, varsToSkip []string, Stdin io.ReadCloser, Stdout io.WriteCloser) (map[string]string, error) {
+func RunPromptsFromConfigWithSkipsIO(draftConfig *config.DraftConfig, varsToSkip []string, Stdin io.ReadCloser, Stdout io.WriteCloser) (map[string]string, error) {
 	skipMap := make(map[string]interface{})
 	for _, v := range varsToSkip {
 		skipMap[v] = interface{}(nil)
@@ -39,7 +39,7 @@ func RunPromptsFromConfigWithSkipsIO(config *config.DraftConfig, varsToSkip []st
 
 	inputs := make(map[string]string)
 
-	for _, variable := range config.Variables {
+	for _, variable := range draftConfig.Variables {
 		if val, ok := skipMap[variable.Name]; ok && val != "" {
 			log.Debugf("Skipping prompt for %s", variable.Name)
 			continue
--- pkg/workflows/workflowconfig.go
+++ pkg/workflows/workflowconfig.go
@@ -1,16 +1,33 @@
 package workflows
 
 type WorkflowConfig struct {
-	AcrName           string
-	ContainerName     string
-	ResourceGroupName string
-	AksClusterName    string
-	BranchName        string
-	BuildContextPath  string
+	WorkflowName         string
+	BranchName           string
+	AcrResourceGroup     string
+	AcrName              string
+	ContainerName        string
+	ClusterResourceGroup string
+	ClusterName          string
+	Dockerfile           string
+	BuildContextPath     string
+	Namespace            string
+	PrivateCluster       string
 }
 
 func (config *WorkflowConfig) SetFlagValuesToMap() map[string]string {
 	flagValuesMap := make(map[string]string)
+	if config.WorkflowName != "" {
+		flagValuesMap["WORKFLOWNAME"] = config.WorkflowName
+	}
+
+	if config.BranchName != "" {
+		flagValuesMap["BRANCHNAME"] = config.BranchName
+	}
+
+	if config.AcrResourceGroup != "" {
+		flagValuesMap["ACRRESOURCEGROUP"] = config.AcrResourceGroup
+	}
+
 	if config.AcrName != "" {
 		flagValuesMap["AZURECONTAINERREGISTRY"] = config.AcrName
 	}
@@ -19,20 +36,28 @@ func (config *WorkflowConfig) SetFlagValuesToMap() map[string]string {
 		flagValuesMap["CONTAINERNAME"] = config.ContainerName
 	}
 
-	if config.ResourceGroupName != "" {
-		flagValuesMap["RESOURCEGROUP"] = config.ResourceGroupName
+	if config.ClusterResourceGroup != "" {
+		flagValuesMap["CLUSTERRESOURCEGROUP"] = config.ClusterResourceGroup
 	}
 
-	if config.AksClusterName != "" {
-		flagValuesMap["CLUSTERNAME"] = config.AksClusterName
+	if config.ClusterName != "" {
+		flagValuesMap["CLUSTERNAME"] = config.ClusterName
 	}
 
-	if config.BranchName != "" {
-		flagValuesMap["BRANCHNAME"] = config.BranchName
+	if config.Dockerfile != "" {
+		flagValuesMap["DOCKERFILE"] = config.Dockerfile
 	}
 
 	if config.BuildContextPath != "" {
 		flagValuesMap["BUILDCONTEXTPATH"] = config.BuildContextPath
+	}
+
+	if config.Namespace != "" {
+		flagValuesMap["NAMESPACE"] = config.Namespace
+	}
+
+	if config.PrivateCluster != "" {
+		flagValuesMap["PRIVATECLUSTER"] = config.PrivateCluster
 	}
 
 	return flagValuesMap

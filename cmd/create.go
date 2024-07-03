71b37615c619f19d711dfdcea503d1de639c02c7
t-mferrari <t-mferrari@microsoft.com>
2024-07-03 14:25:50 -0400

    clean up

--- cmd/create.go
+++ cmd/create.go
@@ -462,14 +462,9 @@ func validateConfigInputsToPrompts(draftConfig *config.DraftConfig, provided []U
 		if err != nil {
 			log.Infof("adding new environment argument %s", providedVar.Name)
 			draftConfig.AddVariable(providedVar.Name, providedVar.Value)
-			variable, err = draftConfig.GetVariable(providedVar.Name)
-			if err != nil {
-				return err
-			}
+			variable, _ = draftConfig.GetVariable(providedVar.Name)
 		}
-		if variable == nil {
-			return fmt.Errorf("variable %s not found", providedVar.Name)
-		}
+
 		variable.Value = providedVar.Value
 	}
 

ab1a1a7ae29c687bbb1fd46f86a29d5d651fe6ba
Suneha Bose <123775811+bosesuneha@users.noreply.github.com>
2024-08-13 12:58:52 -0700

    Simplify namespace creation for helm deployment (#365)

--- test/check_windows_helm.ps1
+++ test/check_windows_helm.ps1
@@ -9,8 +9,6 @@ $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/deployment.yaml
 echo "$file exists: $filesExist"
 $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/service.yaml -PathType Leaf)
 echo "$file exists: $filesExist"
-$filesExist=$filesExist -and (Test-Path -Path ./charts/templates/namespace.yaml -PathType Leaf)
-echo "$file exists: $filesExist"
 $filesExist=$filesExist -and (Test-Path -Path ./charts/templates/_helpers.tpl -PathType Leaf)
 echo "$file exists: $filesExist"
 $filesExist=$filesExist -and (Test-Path -Path ./charts/values.yaml -PathType Leaf)

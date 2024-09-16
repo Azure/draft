08249af2ff510e5dfde71d79361bb3f2e9979aa4
Brandon Foley <brandonfoley13@gmail.com>
2024-09-16 11:07:10 -0400

    Add ns to deployment

--- pkg/template/handler_test.go
+++ pkg/template/handler_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 type FakeTemplateWriter struct {
-	filesWritten       map[string][]byte
+	filesWritten map[string][]byte
 }
 
 func (m *FakeTemplateWriter) EnsureDirectory(dirPath string) error {
@@ -24,36 +24,36 @@ func (m *FakeTemplateWriter) WriteFile(filePath string, content []byte) error {
 
 func TestGetTemplate(t *testing.T) {
 	tests := []struct {
-		name string
-		version string
+		name         string
+		version      string
 		templateName string
-		destination string
-		tmplWriter *FakeTemplateWriter
-		expectedErr bool
+		destination  string
+		tmplWriter   *FakeTemplateWriter
+		expectedErr  bool
 	}{
 		{
-			name: "successful 0.0.1 copy",
-			version: "0.0.1",
+			name:         "successful 0.0.1 copy",
+			version:      "0.0.1",
 			templateName: "Deployment",
-			destination: "./manifests",
-			tmplWriter: &FakeTemplateWriter{},
-			expectedErr: false,
+			destination:  "./manifests",
+			tmplWriter:   &FakeTemplateWriter{},
+			expectedErr:  false,
 		},
 		{
-			name: "invalid template name",
-			version: "0.0.1",
+			name:         "invalid template name",
+			version:      "0.0.1",
 			templateName: "InvalidTemplate",
-			destination: "./manifests",
-			tmplWriter: &FakeTemplateWriter{},
-			expectedErr: true,
+			destination:  "./manifests",
+			tmplWriter:   &FakeTemplateWriter{},
+			expectedErr:  true,
 		},
 		{
-			name: "invalid template version",
-			version: "1.0.0",
+			name:         "invalid template version",
+			version:      "1.0.0",
 			templateName: "Deployment",
-			destination: "./manifests",
-			tmplWriter: &FakeTemplateWriter{},
-			expectedErr: true,
+			destination:  "./manifests",
+			tmplWriter:   &FakeTemplateWriter{},
+			expectedErr:  true,
 		},
 	}
 
@@ -73,42 +73,42 @@ func TestGetTemplate(t *testing.T) {
 
 func TestSuccessfulVersions(t *testing.T) {
 	tests := []struct {
-		name string
-		version string
-		cpuIn string
-		memIn string
-		tmplWriter *FakeTemplateWriter
+		name           string
+		version        string
+		cpuIn          string
+		memIn          string
+		tmplWriter     *FakeTemplateWriter
 		expectedOutput string
 	}{
 		{
-			name: "successful 0.0.1 copy",
-			version: "0.0.1",
-			tmplWriter: &FakeTemplateWriter{},
-			expectedOutput: "apiVersion: apps/v1 \nkind: Deployment\nmetadata:\n name: test\n labels:\n  app.kubernetes.io/name: test\n  app.kubernetes.io/part-of: test\n  kubernetes.azure.com/generator: draft\nspec:\n replicas: 2\n selector:\n  matchLabels:\n   app: test\n template:\n  metadata:\n   labels:\n    app: test\n  spec:\n   containers:\n    - name: test\n      image: test:latest\n      imagePullPolicy: IfNotPresent\n      ports:\n      - containerPort: 80\n      securityContext:\n        privileged: false\n        allowPrivilegeEscalation: false\n        runAsUser: 1000\n        runAsGroup: 3000\n        runAsNonRoot: true\n        readOnlyRootFilesystem: true\n        capabilities:\n        drop:\n          - all\n        add:\n          - SETPCAP\n          - MKNOD\n          - AUDIT_WRITE\n          - CHOWN\n          - DAC_OVERRIDE\n          - FOWNER\n          - FSETID\n          - KILL\n          - SETGID\n          - SETUID\n          - NET_BIND_SERVICE\n          - SYS_CHROOT\n          - SETFCAP\n          - SYS_PTRACE\n  affinity:\n    podAntiAffinity:\n      preferredDuringSchedulingIgnoredDuringExecution:\n      - weight: 100\n        podAffinityTerm:\n          topologyKey: kubernetes.io/hostname\n          labelSelector:\n            matchLabels:\n            app: test\n    topologySpreadConstraints:\n      - maxSkew: 1\n        topologyKey: topology.kubernetes.io/zone\n        whenUnsatisfiable: ScheduleAnyway\n        labelSelector:\n          matchLabels:\n          app: test\n    hostNetwork: false\n    hostIPC: false\n    securityContext:\n      seccompProfile:\n      type: RuntimeDefault",
+			name:           "successful 0.0.1 copy",
+			version:        "0.0.1",
+			tmplWriter:     &FakeTemplateWriter{},
+			expectedOutput: "apiVersion: apps/v1 \nkind: Deployment\nmetadata:\n name: test\n namespace: test\n labels:\n  app.kubernetes.io/name: test\n  app.kubernetes.io/part-of: test\n  kubernetes.azure.com/generator: draft\nspec:\n replicas: 2\n selector:\n  matchLabels:\n   app: test\n template:\n  metadata:\n   labels:\n    app: test\n  spec:\n   containers:\n    - name: test\n      image: test:latest\n      imagePullPolicy: IfNotPresent\n      ports:\n      - containerPort: 80\n      securityContext:\n        privileged: false\n        allowPrivilegeEscalation: false\n        runAsUser: 1000\n        runAsGroup: 3000\n        runAsNonRoot: true\n        readOnlyRootFilesystem: true\n        capabilities:\n        drop:\n          - all\n        add:\n          - SETPCAP\n          - MKNOD\n          - AUDIT_WRITE\n          - CHOWN\n          - DAC_OVERRIDE\n          - FOWNER\n          - FSETID\n          - KILL\n          - SETGID\n          - SETUID\n          - NET_BIND_SERVICE\n          - SYS_CHROOT\n          - SETFCAP\n          - SYS_PTRACE\n  affinity:\n    podAntiAffinity:\n      preferredDuringSchedulingIgnoredDuringExecution:\n      - weight: 100\n        podAffinityTerm:\n          topologyKey: kubernetes.io/hostname\n          labelSelector:\n            matchLabels:\n            app: test\n    topologySpreadConstraints:\n      - maxSkew: 1\n        topologyKey: topology.kubernetes.io/zone\n        whenUnsatisfiable: ScheduleAnyway\n        labelSelector:\n          matchLabels:\n          app: test\n    hostNetwork: false\n    hostIPC: false\n    securityContext:\n      seccompProfile:\n      type: RuntimeDefault",
 		},
 		{
-			name: "successful 0.0.2 copy",
-			version: "0.0.2",
-			cpuIn: "1",
-			memIn: "1Gi",
-			tmplWriter: &FakeTemplateWriter{},
-			expectedOutput: "apiVersion: apps/v1 \nkind: Deployment\nmetadata:\n name: test\n labels:\n  app.kubernetes.io/name: test\n  app.kubernetes.io/part-of: test\n  kubernetes.azure.com/generator: draft\nspec:\n replicas: 2\n selector:\n  matchLabels:\n   app: test\n template:\n  metadata:\n   labels:\n    app: test\n  spec:\n   containers:\n    - name: test\n      image: test:latest\n      imagePullPolicy: IfNotPresent\n      ports:\n      - containerPort: 80\n      resources:\n        limits:\n          cpu: 1\n          memory: 1Gi\n        requests:\n          cpu: 1\n          memory: 1Gi\n      securityContext:\n        privileged: false\n        allowPrivilegeEscalation: false\n        runAsUser: 1000\n        runAsGroup: 3000\n        runAsNonRoot: true\n        readOnlyRootFilesystem: true\n        capabilities:\n        drop:\n          - all\n        add:\n          - SETPCAP\n          - MKNOD\n          - AUDIT_WRITE\n          - CHOWN\n          - DAC_OVERRIDE\n          - FOWNER\n          - FSETID\n          - KILL\n          - SETGID\n          - SETUID\n          - NET_BIND_SERVICE\n          - SYS_CHROOT\n          - SETFCAP\n          - SYS_PTRACE\n  affinity:\n    podAntiAffinity:\n      preferredDuringSchedulingIgnoredDuringExecution:\n      - weight: 100\n        podAffinityTerm:\n          topologyKey: kubernetes.io/hostname\n          labelSelector:\n            matchLabels:\n            app: test\n    topologySpreadConstraints:\n      - maxSkew: 1\n        topologyKey: topology.kubernetes.io/zone\n        whenUnsatisfiable: ScheduleAnyway\n        labelSelector:\n          matchLabels:\n          app: test\n    hostNetwork: false\n    hostIPC: false\n    securityContext:\n      seccompProfile:\n      type: RuntimeDefault",
+			name:           "successful 0.0.2 copy",
+			version:        "0.0.2",
+			cpuIn:          "1",
+			memIn:          "1Gi",
+			tmplWriter:     &FakeTemplateWriter{},
+			expectedOutput: "apiVersion: apps/v1 \nkind: Deployment\nmetadata:\n name: test\n namespace: test\n labels:\n  app.kubernetes.io/name: test\n  app.kubernetes.io/part-of: test\n  kubernetes.azure.com/generator: draft\nspec:\n replicas: 2\n selector:\n  matchLabels:\n   app: test\n template:\n  metadata:\n   labels:\n    app: test\n  spec:\n   containers:\n    - name: test\n      image: test:latest\n      imagePullPolicy: IfNotPresent\n      ports:\n      - containerPort: 80\n      resources:\n        limits:\n          cpu: 1\n          memory: 1Gi\n        requests:\n          cpu: 1\n          memory: 1Gi\n      securityContext:\n        privileged: false\n        allowPrivilegeEscalation: false\n        runAsUser: 1000\n        runAsGroup: 3000\n        runAsNonRoot: true\n        readOnlyRootFilesystem: true\n        capabilities:\n        drop:\n          - all\n        add:\n          - SETPCAP\n          - MKNOD\n          - AUDIT_WRITE\n          - CHOWN\n          - DAC_OVERRIDE\n          - FOWNER\n          - FSETID\n          - KILL\n          - SETGID\n          - SETUID\n          - NET_BIND_SERVICE\n          - SYS_CHROOT\n          - SETFCAP\n          - SYS_PTRACE\n  affinity:\n    podAntiAffinity:\n      preferredDuringSchedulingIgnoredDuringExecution:\n      - weight: 100\n        podAffinityTerm:\n          topologyKey: kubernetes.io/hostname\n          labelSelector:\n            matchLabels:\n            app: test\n    topologySpreadConstraints:\n      - maxSkew: 1\n        topologyKey: topology.kubernetes.io/zone\n        whenUnsatisfiable: ScheduleAnyway\n        labelSelector:\n          matchLabels:\n          app: test\n    hostNetwork: false\n    hostIPC: false\n    securityContext:\n      seccompProfile:\n      type: RuntimeDefault",
 		},
 		{
-			name: "successful 0.0.3 copy",
-			version: "0.0.3",
-			cpuIn: "1",
-			memIn: "1Gi",
-			tmplWriter: &FakeTemplateWriter{},
-			expectedOutput: "apiVersion: apps/v1 \nkind: Deployment\nmetadata:\n name: test\n labels:\n  app.kubernetes.io/name: test\n  app.kubernetes.io/part-of: test\n  kubernetes.azure.com/generator: draft\nspec:\n replicas: 2\n selector:\n  matchLabels:\n   app: test\n template:\n  metadata:\n   labels:\n    app: test\n  spec:\n   containers:\n    - name: test\n      image: test:latest\n      imagePullPolicy: IfNotPresent\n      ports:\n      - containerPort: 80\n      resources:\n        limits:\n          cpu: 1\n          memory: 1Gi\n        requests:\n          cpu: 1\n          memory: 1Gi\n      livenessProbe:\n        tcpSocket:\n          port: 80\n      readinessProbe:\n        tcpSocket:\n          port: 80\n        periodSeconds: 1\n        timeoutSeconds: 5\n      securityContext:\n        privileged: false\n        allowPrivilegeEscalation: false\n        runAsUser: 1000\n        runAsGroup: 3000\n        runAsNonRoot: true\n        readOnlyRootFilesystem: true\n        capabilities:\n        drop:\n          - all\n        add:\n          - SETPCAP\n          - MKNOD\n          - AUDIT_WRITE\n          - CHOWN\n          - DAC_OVERRIDE\n          - FOWNER\n          - FSETID\n          - KILL\n          - SETGID\n          - SETUID\n          - NET_BIND_SERVICE\n          - SYS_CHROOT\n          - SETFCAP\n          - SYS_PTRACE\n  affinity:\n    podAntiAffinity:\n      preferredDuringSchedulingIgnoredDuringExecution:\n      - weight: 100\n        podAffinityTerm:\n          topologyKey: kubernetes.io/hostname\n          labelSelector:\n            matchLabels:\n            app: test\n    topologySpreadConstraints:\n      - maxSkew: 1\n        topologyKey: topology.kubernetes.io/zone\n        whenUnsatisfiable: ScheduleAnyway\n        labelSelector:\n          matchLabels:\n          app: test\n    hostNetwork: false\n    hostIPC: false\n    securityContext:\n      seccompProfile:\n      type: RuntimeDefault",
+			name:           "successful 0.0.3 copy",
+			version:        "0.0.3",
+			cpuIn:          "1",
+			memIn:          "1Gi",
+			tmplWriter:     &FakeTemplateWriter{},
+			expectedOutput: "apiVersion: apps/v1 \nkind: Deployment\nmetadata:\n name: test\n namespace: test\n labels:\n  app.kubernetes.io/name: test\n  app.kubernetes.io/part-of: test\n  kubernetes.azure.com/generator: draft\nspec:\n replicas: 2\n selector:\n  matchLabels:\n   app: test\n template:\n  metadata:\n   labels:\n    app: test\n  spec:\n   containers:\n    - name: test\n      image: test:latest\n      imagePullPolicy: IfNotPresent\n      ports:\n      - containerPort: 80\n      resources:\n        limits:\n          cpu: 1\n          memory: 1Gi\n        requests:\n          cpu: 1\n          memory: 1Gi\n      livenessProbe:\n        tcpSocket:\n          port: 80\n      readinessProbe:\n        tcpSocket:\n          port: 80\n        periodSeconds: 1\n        timeoutSeconds: 5\n      securityContext:\n        privileged: false\n        allowPrivilegeEscalation: false\n        runAsUser: 1000\n        runAsGroup: 3000\n        runAsNonRoot: true\n        readOnlyRootFilesystem: true\n        capabilities:\n        drop:\n          - all\n        add:\n          - SETPCAP\n          - MKNOD\n          - AUDIT_WRITE\n          - CHOWN\n          - DAC_OVERRIDE\n          - FOWNER\n          - FSETID\n          - KILL\n          - SETGID\n          - SETUID\n          - NET_BIND_SERVICE\n          - SYS_CHROOT\n          - SETFCAP\n          - SYS_PTRACE\n  affinity:\n    podAntiAffinity:\n      preferredDuringSchedulingIgnoredDuringExecution:\n      - weight: 100\n        podAffinityTerm:\n          topologyKey: kubernetes.io/hostname\n          labelSelector:\n            matchLabels:\n            app: test\n    topologySpreadConstraints:\n      - maxSkew: 1\n        topologyKey: topology.kubernetes.io/zone\n        whenUnsatisfiable: ScheduleAnyway\n        labelSelector:\n          matchLabels:\n          app: test\n    hostNetwork: false\n    hostIPC: false\n    securityContext:\n      seccompProfile:\n      type: RuntimeDefault",
 		},
 		{
-			name: "successful 0.0.4 copy",
-			version: "0.0.4",
-			cpuIn: "1",
-			memIn: "1Gi",
-			tmplWriter: &FakeTemplateWriter{},
-			expectedOutput: "apiVersion: apps/v1 \nkind: Deployment\nmetadata:\n name: test\n labels:\n  app.kubernetes.io/name: test\n  app.kubernetes.io/part-of: test\n  kubernetes.azure.com/generator: draft\nspec:\n replicas: 2\n selector:\n  matchLabels:\n   app: test\n template:\n  metadata:\n   labels:\n    app: test\n  spec:\n   containers:\n    - name: test\n      image: test:latest\n      imagePullPolicy: IfNotPresent\n      ports:\n      - containerPort: 80\n      resources:\n        limits:\n          cpu: 1\n          memory: 1Gi\n        requests:\n          cpu: 1\n          memory: 1Gi\n      livenessProbe:\n        tcpSocket:\n          port: 80\n      readinessProbe:\n        tcpSocket:\n          port: 80\n        periodSeconds: 1\n        timeoutSeconds: 5\n        initialDelaySeconds: 3\n        successThreshold: 1 \n        failureThreshold: 1\n      securityContext:\n        privileged: false\n        allowPrivilegeEscalation: false\n        runAsUser: 1000\n        runAsGroup: 3000\n        runAsNonRoot: true\n        readOnlyRootFilesystem: true\n        capabilities:\n        drop:\n          - all\n        add:\n          - SETPCAP\n          - MKNOD\n          - AUDIT_WRITE\n          - CHOWN\n          - DAC_OVERRIDE\n          - FOWNER\n          - FSETID\n          - KILL\n          - SETGID\n          - SETUID\n          - NET_BIND_SERVICE\n          - SYS_CHROOT\n          - SETFCAP\n          - SYS_PTRACE\n  affinity:\n    podAntiAffinity:\n      preferredDuringSchedulingIgnoredDuringExecution:\n      - weight: 100\n        podAffinityTerm:\n          topologyKey: kubernetes.io/hostname\n          labelSelector:\n            matchLabels:\n            app: test\n    topologySpreadConstraints:\n      - maxSkew: 1\n        topologyKey: topology.kubernetes.io/zone\n        whenUnsatisfiable: ScheduleAnyway\n        labelSelector:\n          matchLabels:\n          app: test\n    hostNetwork: false\n    hostIPC: false\n    securityContext:\n      seccompProfile:\n      type: RuntimeDefault",
+			name:           "successful 0.0.4 copy",
+			version:        "0.0.4",
+			cpuIn:          "1",
+			memIn:          "1Gi",
+			tmplWriter:     &FakeTemplateWriter{},
+			expectedOutput: "apiVersion: apps/v1 \nkind: Deployment\nmetadata:\n name: test\n namespace: test\n labels:\n  app.kubernetes.io/name: test\n  app.kubernetes.io/part-of: test\n  kubernetes.azure.com/generator: draft\nspec:\n replicas: 2\n selector:\n  matchLabels:\n   app: test\n template:\n  metadata:\n   labels:\n    app: test\n  spec:\n   containers:\n    - name: test\n      image: test:latest\n      imagePullPolicy: IfNotPresent\n      ports:\n      - containerPort: 80\n      resources:\n        limits:\n          cpu: 1\n          memory: 1Gi\n        requests:\n          cpu: 1\n          memory: 1Gi\n      livenessProbe:\n        tcpSocket:\n          port: 80\n      readinessProbe:\n        tcpSocket:\n          port: 80\n        periodSeconds: 1\n        timeoutSeconds: 5\n        initialDelaySeconds: 3\n        successThreshold: 1 \n        failureThreshold: 1\n      securityContext:\n        privileged: false\n        allowPrivilegeEscalation: false\n        runAsUser: 1000\n        runAsGroup: 3000\n        runAsNonRoot: true\n        readOnlyRootFilesystem: true\n        capabilities:\n        drop:\n          - all\n        add:\n          - SETPCAP\n          - MKNOD\n          - AUDIT_WRITE\n          - CHOWN\n          - DAC_OVERRIDE\n          - FOWNER\n          - FSETID\n          - KILL\n          - SETGID\n          - SETUID\n          - NET_BIND_SERVICE\n          - SYS_CHROOT\n          - SETFCAP\n          - SYS_PTRACE\n  affinity:\n    podAntiAffinity:\n      preferredDuringSchedulingIgnoredDuringExecution:\n      - weight: 100\n        podAffinityTerm:\n          topologyKey: kubernetes.io/hostname\n          labelSelector:\n            matchLabels:\n            app: test\n    topologySpreadConstraints:\n      - maxSkew: 1\n        topologyKey: topology.kubernetes.io/zone\n        whenUnsatisfiable: ScheduleAnyway\n        labelSelector:\n          matchLabels:\n          app: test\n    hostNetwork: false\n    hostIPC: false\n    securityContext:\n      seccompProfile:\n      type: RuntimeDefault",
 		},
 	}
 
@@ -118,6 +118,7 @@ func TestSuccessfulVersions(t *testing.T) {
 			assert.NoError(t, err)
 
 			deploymentTemplate.Config.SetVariable("APPNAME", "test")
+			deploymentTemplate.Config.SetVariable("NAMESPACE", "test")
 			deploymentTemplate.Config.SetVariable("CPULIMIT", tt.cpuIn)
 			deploymentTemplate.Config.SetVariable("MEMLIMIT", tt.memIn)
 			deploymentTemplate.Config.SetVariable("CPUREQ", tt.cpuIn)

540b14058711f1eb574b389038aa85837ea8c141
Vidya Reddy <59590642+Vidya2606@users.noreply.github.com>
2024-09-23 13:55:48 -0700

    migrate createRole assignment to Az SDK

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.14.0
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.7.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/authorization/armauthorization v1.0.0
 	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/subscription/armsubscription v1.2.0
 	github.com/briandowns/spinner v1.23.1
 	github.com/cenkalti/backoff/v4 v4.3.0
@@ -23,6 +24,7 @@ require (
 	go.uber.org/mock v0.4.0
 	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f
 	golang.org/x/mod v0.20.0
+	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.1
 	helm.sh/helm/v3 v3.14.4
 	k8s.io/api v0.29.3
@@ -153,7 +155,6 @@ require (
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	k8s.io/apiextensions-apiserver v0.29.3 // indirect
 	k8s.io/apiserver v0.29.3 // indirect
 	k8s.io/component-base v0.29.3 // indirect
--- pkg/providers/az-client.go
+++ pkg/providers/az-client.go
@@ -1,15 +1,32 @@
 package providers
 
 import (
+	"fmt"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/authorization/armauthorization"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/subscription/armsubscription"
+	"github.com/Azure/draft/pkg/cred"
 )
 
 type AzClient struct {
-	AzTenantClient azTenantClient
+	AzTenantClient   azTenantClient
+	RoleAssignClient *armauthorization.RoleAssignmentsClient
 }
 
 //go:generate mockgen -source=./az-client.go -destination=./mock/az-client.go .
 type azTenantClient interface {
 	NewListPager(options *armsubscription.TenantsClientListOptions) *runtime.Pager[armsubscription.TenantsClientListResponse]
+}
+
+func createRoleAssignmentClient(subscriptionId string) (*armauthorization.RoleAssignmentsClient, error) {
+	cred, err := cred.GetCred()
+	if err != nil {
+		return nil, fmt.Errorf("getting credentials: %w", err)
+	}
+
+	client, err := armauthorization.NewRoleAssignmentsClient(subscriptionId, cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("creating role assignment client: %w", err)
+	}
+	return client, nil
 }
